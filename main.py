1937f5a475092b6357f9f42236ca13db0079d365
Khalfani Bozeman <84476080+egrep6021ad@users.noreply.github.com>
2022-03-01 23:38:08 +0000

    should be done

--- main.py
+++ main.py
@@ -1,124 +1,99 @@
 import hashlib
-while(True):
-  user_id = int(input("\nWhat is your 3 digit UID?\n"))
-  temp_pass = input("Whis is your 4 digit password?\n")
-  is_validated = False
-    # check if user is entered a password
-  entered_password=False
-    # if user entered a password
-    #is_validated = False
-  if len(temp_pass) > 3:
-      entered_password = True
-      temp_pass = int(temp_pass)
-    
-  
-  def main():
-    is_validated = False
-    real_pass = ''
-    real_salt = ''
-    show_source = ''
-    my_bool = 0
-    check_hash= 0
-    with open("UID.txt") as user_file:
+while(True):   
+
+  # Main password cracker  
+  def main(is_validated):
+    with open("UID.txt") as user_file: # For all lines in UID.txt
       i = 0
       for x in user_file: 
+         if is_validated == True:
+           break
          i = i + 1
-         k = 0
-         show_source=''
-        # k = x
-         if int(x) == user_id: # If user matches a user
-          with open("Password.txt") as fp: # Open password db
+         if int(x) == user_id:         # If user input matches UID entry
+          with open("Password.txt") as fp: # For all lines in UID.txt
             for y in fp:
-                if temp_pass == int(y):   # If password matches that user
-                  if my_bool < 1:
-                   my_bool = my_bool + 1
-                   real_pass = temp_pass
-                  with open("Salt.txt",encoding='utf8') as salt: # open salts db
-                   j = 1
-                   for el in salt: 
-                     if (j == i):
-                       temp_salt = str(el.strip())
-                       if my_bool < 2:
-                         my_bool = my_bool + 1
-                       hash = y.strip('\n')+temp_salt # password+salt =>
-                       m = hashlib.md5()
-                       m.update(hash.encode('utf-8')) # = GENERATE hash code
-                       hash_code = m.hexdigest() 
-                     
-                       
-                       with open("Hash.txt",encoding='utf8') as f: # open hashes db
-                        for z in f:
-                          password = y
-                          source_hash = z.strip()
-                          if source_hash == hash_code:  # check hash against db hash
-                            if check_hash < 1:
-                             show_source = source_hash
-                             print(show_source)
-                             check_hash= 1
-                            if my_bool < 3:
-                             is_validated = True
-                             show_source = source_hash
-                             real_salt = temp_salt 
-                             
-                             my_bool = my_bool + 1
-                             
-                            break
-                     else :# End salt loop
-                       j = j + 1    
+              if temp_pass == int(y):   # If password matches password entry
+                with open("Salt.txt",encoding='utf8') as salt: #
+                 j = 1
+                 for el in salt:                    # For all lines in Salt.txt
+                   if (j == i):                     # If the current line is in the sme row as the UID's row
+                     temp_salt = str(el.strip())
+                     hash = y.strip('\n')+temp_salt # password+salt => hash function
+                     m = hashlib.md5()
+                     m.update(hash.encode('utf-8')) #  GENERATE hash code
+                     hash_code = m.hexdigest() 
+                     with open("Hash.txt",encoding='utf8') as f: # For all lines in Hash.txt
+                      for z in f:
+                        if is_validated == True:                 # Optimize
+                         break
+                        source_hash = z.strip()
+                        if source_hash == hash_code:             # Compare current lines hash with generated hash
+                          print(f'\n Source  hash : {source_hash}')   # Print source hash_code
+                          print(f'Generated hash: {hash_code}')     # Print generated hash_code
+                          is_validated = True                    # Mark user as validated
+                          break
+                   else : # Otherwise, go to next salt entry
+                     j = j + 1   
       # End outter for-loop                 
-      return (real_pass, real_salt, show_source, is_validated)
+      return is_validated
   #End Main
-  def p_cracker():
-    found_pass =''
-    found_salt =''
-    
-    i = 0 
-    with open("UID.txt") as user_file:
-     for x in user_file:
-       if int(x) == user_id:
-         break
-         continue
+  def p_cracker(found_pass, found_salt, i):
+    with open("UID.txt") as user_file: # For all lines in UID.txt
+     for x in user_file:              
+       if int(x) == user_id:           # If current line matches user input
+         break                         # Break such that i = the line number
+         #continue                      
        else :
           i = i + 1
-     j = 0
-     with open("Password.txt") as fp:
-      for x in fp:
-       if j == i:
-         found_pass = x
+     j = 0                             # Set new line counter
+     with open("Password.txt") as fp:  # For all lines in Password.txt
+      for x in fp:          
+       if j == i:                      # When the line numbers are the same
+         found_pass = x                # The password has been found
          break
-         continue
+         #continue
        else:
          j = j + 1
-      k = 0
-      with open("Salt.txt") as f:
+      k = 0                            # Set new line counter
+      with open("Salt.txt") as f:      # For all lines in Salt.txt
         for x in f:
-         if k == i:
-          found_salt = x
-          break
+         if k == i:                    # When the line number is the same as the UID line row
+          found_salt = x               # The salt has been found
+          break  
          else:
            k = k + 1
-    return(found_pass, found_salt)
-  
+    return(found_pass, found_salt)     # Return the users password and salt 
+
+
     
-  if entered_password == True:
-    arr = main()
-    real_pass = arr[0]
-    real_salt = arr[1]
-    show_source = arr[2]
-    print(show_source)
-    print('The input password and salt MATCHES the hash value in the database\n')
-    answer = input("Another login? (y/n) \n")
+
+  # Get user input
+  user_id = int(input("\nWhat is your 3 digit UID?\n"))
+  temp_pass = input("Whis is your 4 digit password?\n")
+
+  # If the user entered a password make it an integer
+  if len(temp_pass) > 3:
+      temp_pass = int(temp_pass)
+  # Check validation
+  is_validated = main(False)
+  # If the user is validated:
+  if is_validated:
+    print('\nThe input password and salt MATCHES the hash value in the database')
+    answer = input("\nAnother login? (y/n) \n")
+    is_validated = False
     if answer != 'y':
       break
-   
   else:
+    # If the user is NOT validated, give them a choice:
     print('The input password and salt does not match the hash value in the database...\n')
     answer = input("Crack it? ( y/n ) \n")
+    # If user chose to crack password:
     if(answer=='y'):
-      arr = p_cracker()
+      arr = p_cracker('','',0)
       print("Password: "+arr[0] +'\nSalt: '+arr[1]) 
+    # Else allow them to attempt again
     else:
-      break
+      continue
     
   
 
